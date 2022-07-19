1f74828474db505460f9a6b203b5a8416afec187
Martin Palmowski <martin.palmowski@gmail.com>
2022-07-19 14:47:44 +0200

    Switched to nicer colorscheme.

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -60,9 +60,10 @@ return packer.startup(function(use)
   use "folke/which-key.nvim"
 
   -- Colorschemes
-  use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
+  -- use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
   use "lunarvim/darkplus.nvim"
   use "ellisonleao/gruvbox.nvim"
+  use "folke/tokyonight.nvim"
 
   -- cmp plugins
   use "hrsh7th/nvim-cmp" -- The completion plugin
