fd3e39be38091b0b31e7cdae5b993197f4b19db4
Martin Palmowski <martin.palmowski@gmail.com>
2022-07-21 16:13:18 +0200

    Added comment what the plugin is for.

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -49,7 +49,7 @@ return packer.startup(function(use)
   use "kyazdani42/nvim-web-devicons"
   use "kyazdani42/nvim-tree.lua"
   use "akinsho/bufferline.nvim"
-  use "moll/vim-bbye"
+  use "moll/vim-bbye" -- Bbye allows you to do delete buffers (close files) without closing your windows or messing up your layout.
   use "nvim-lualine/lualine.nvim"
   use "akinsho/toggleterm.nvim"
   use "ahmedkhalf/project.nvim"
